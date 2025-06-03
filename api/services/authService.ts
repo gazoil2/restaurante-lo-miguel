f749f13a610d750ab4ecb54b59d4fc5e70c773fd
niitroxendioxide <niitroxendioxide@gmail.com>
2025-06-02 21:31:08 -0300

    Kys

--- api/services/authService.ts
+++ api/services/authService.ts
@@ -1,9 +1,12 @@
 import { $db } from "../db";
 
+
+
 export const checkUserIsAdmin = async (userId: number): Promise<boolean> => {
   const admin = await $db.admin.findFirst({
     where: { idUser: userId },
   });
+
 
   return admin ? true : false;
 }
